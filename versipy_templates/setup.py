f77950c3a7de5749b7a156dd87ca2de85557e43c
snajder <rene.snajder@gmail.com>
2021-05-19 10:21:12 +0200

    Versipy templates

---
+++ versipy_templates/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+# Define package info
+name = ""
+description = "DNA methylation analysis downstream to Nanopolish for Oxford Nanopore DNA sequencing datasets"
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# Collect info in a dictionnary for setup.py
+setup(
+    name="__package_name__",
+    description="__package_description__",
+    version="__package_version__",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="__package_url__",
+    author="__author_name__",
+    author_email="__author_email__",
+    license="__package_licence__",
+    python_requires=">=__minimal_python__",
+    classifiers=[
+        __@{,
+        ::"classifiers"}__
+    ],
+    install_requires=[
+        __@{,
+        ::"dependencies"}__
+    ],
+    packages=[name],
+    package_dir={name: name},
+    package_data={name: ["templates/*"]},
+    entry_points={"console_scripts": ["pycoMeth=pycoMeth.__main__:main"]},
+)
